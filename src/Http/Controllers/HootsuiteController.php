c10b2d61fafc3d95a4c539cad1c8d2ded662baa0
Guy-roland ASSALE <rolandassale@gmail.com>
2021-01-28 13:38:02 +0000

    fix home controller issue

--- src/Http/Controllers/HootsuiteController.php
+++ src/Http/Controllers/HootsuiteController.php
@@ -11,22 +11,21 @@ class HootsuiteController
     public function saveTokens(Request $request, HootsuiteSettings $settings)
     {
 
-        // dd($settings);
 
-        // if (
-        //      !$request->has('access_token') || !$request->get('refresh_token') || !$request->get('expires_in')
-        //     )
-        // {
-        //     return;
-        // }
-
+        if (
+             !$request->has('access_token') || !$request->get('refresh_token') || !$request->get('expires_in')
+            )
+        {
+            return;
+        }
 
 
-        // $settings->saveTokens([
-        //     'hootsuite_access_token'  => $request->get('access_token'),
-        //     'hootsuite_refresh_token' => $request->get('refresh_token'),
-        //     'hootsuite_token_expires' => Carbon::now()->addSeconds($request->get('expires_in'))->toDateTimeString(),
-        // ]);
+
+        $settings->saveTokens([
+            'hootsuite_access_token'  => $request->get('access_token'),
+            'hootsuite_refresh_token' => $request->get('refresh_token'),
+            'hootsuite_token_expires' => Carbon::now()->addSeconds($request->get('expires_in'))->toDateTimeString(),
+        ]);
 
         return view('hootsuite::index', compact('settings'));
 
